88fda09651c6c250554cad930b9c2aa360104682
joantorres11 <joan_andres.torres@uao.edu.co>
2025-11-09 21:41:40 -0500

    suufrindno con rl Gui

--- src/main/java/Gestion/VentanaPrincipal.java
+++ src/main/java/Gestion/VentanaPrincipal.java
@@ -1,8 +1,9 @@
+package Gestion;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package Gestion;
+
 
 import Gestion.Reparacion;
 import Gestion.UsaVehiculo;
